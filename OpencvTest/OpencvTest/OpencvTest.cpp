cf99b9ae47d346f6c98671e685a5c4a8385dfb86
Gabriela Estefania Quintanar Yañez <gquintanar277@gmail.com>
2026-01-28 12:26:29 -0600

    Archivos correspondientes para el setup de OpenCV con el código funcional testeado en clase

---
+++ OpencvTest/OpencvTest/OpencvTest.cpp
@@ -0,0 +1,26 @@
+// Opncv_Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+/**
+ * Codigo de prueba para verificar la instalacion y configuracion de OpenCV
+ */
+int main() {
+    Mat img_resized;
+    Mat img = imread("../img/descarga.jpg"); // coloca una imagen en la carpeta del proyecto
+    resize(img, img_resized, Size(636, 316)); // redimensionar a 1024x1024
+    if (img.empty()) {
+        cout << "No se pudo cargar la imagen" << std::endl;
+        return -1;
+    }
+    imshow("Imagen Original", img);
+    imshow("Imagen de prueba", img_resized);
+    waitKey(0);
+    return 0;
+}
+
